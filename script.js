ad1e3253da830734fd6a904632c736b7cf04e8f1
Shaurya <stronglordofhell@gmail.com>
2023-06-11 10:29:59 -0400

    .

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ function goBackToMainPage() {
 function goToLink(link) {
     window.location = link;
 }
+
+function checkInputEmpty(id1, id2, id3) {
+    if (document.getElementById(id1).value == "") {
+        return false;
+    }
+    if (document.getElementById(id2).value == "") {
+        return false;
+    }
+    if (document.getElementById(id3).value == "") {
+        return false;
+    }
+}
